709c013ec17ba996d757fd34187b0411e2d5781b
luisafabri <110108591+luisafabri@users.noreply.github.com>
2022-10-19 11:05:10 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,7 +1,26 @@
+function total(){
+    let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+    for(let i = 1; i <= t; i++){
+        r = c * (1 + (j/100));
+        document.write("Mês " + i + " valor: " + r +"<br>");
+        c = r;
+    }
+    
+    document.write("Resultado: " + r);
+}
 function adicao () {
-    2 let val1 = document . getElementById ("v1") . value ;
-    3 let val2 = document . getElementById ("v2") . value ;
-    4 let r = Number ( val1 ) + Number ( val2 ) ;
-    5 document . getElementById (" resultado ") . innerHTML = r ;
-    6
-    7 }
+     let val1 = document.getElementById("v1").value;
+     let val2 = document.getElementById("v2").value;
+     let r = Number( val1 ) + Number( val2 );
+     document.getElementById(" resultado ").innerHTML = r;
+}
+
+function subtracao () {
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number( val1 ) - Number( val2 );
+    document.getElementById(" resultado ").innerHTML = r;
+}
