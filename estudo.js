b4ba75933ca56231a76f557051334e6fd71ea1a1
luisafabri <110108591+luisafabri@users.noreply.github.com>
2022-10-21 11:59:48 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,15 +1,38 @@
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
+
 function total(){
     let c = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
+    if ( ! Number (c)){
+        alert("O capital tem que ser número, tente novamente.")
+        document . getElementById ("valor").valeu="";
+        document . getElementById ("valor").focus ();
+        return
+    }
+    if ( ! Number (j)){
+        alert("O juros tem que ser números, tente novamente.")
+        document . getElementById ("valor").valeu="";
+        document . getElementById ("valor").focus ();
+        return
+    }
+    if ( ! Number (t)){
+        alert("Os meses tem que ser número, tente novamente.")
+        document . getElementById ("valor").valeu="";
+        document . getElementById ("valor").focus ();
+        return
+    }
     let r = 0;
     for(let i = 1; i <= t; i++){
         r = c * (1 + (j/100));
-        document.write("Mês " + i + " valor: " + r +"<br>");
+        document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
         c = r;
     }
+
     
-    document.write("Resultado: " + r);
+    document.write("Resultado: " + moeda(r));
 }
 function adicao () {
      let val1 = document.getElementById("v1").value;
