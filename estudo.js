2f7efc23f861cd208106ec77e49224a8babe86f9
luisafabri <110108591+luisafabri@users.noreply.github.com>
2022-10-26 11:06:18 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,49 +1,81 @@
 function moeda(atual){
-    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+  return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
 }
+
 
 function total(){
-    let c = document.getElementById("valor").value;
-    let j = document.getElementById("juros").value;
-    let t = document.getElementById("meses").value;
-    if ( ! Number (c)){
-        alert("O capital tem que ser número, tente novamente.")
-        document . getElementById ("valor").valeu="";
-        document . getElementById ("valor").focus ();
-        return
-    }
-    if ( ! Number (j)){
-        alert("O juros tem que ser números, tente novamente.")
-        document . getElementById ("valor").valeu="";
-        document . getElementById ("valor").focus ();
-        return
-    }
-    if ( ! Number (t)){
-        alert("Os meses tem que ser número, tente novamente.")
-        document . getElementById ("valor").valeu="";
-        document . getElementById ("valor").focus ();
-        return
-    }
-    let r = 0;
-    for(let i = 1; i <= t; i++){
-        r = c * (1 + (j/100));
-        document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
-        c = r;
-    }
+  let c = document.getElementById("valor").value;
+  let j = document.getElementById("juros").value;
+  let t = document.getElementById("meses").value;
+  let r = 0;
 
-    
-    document.write("Resultado: " + moeda(r));
-}
-function adicao () {
-     let val1 = document.getElementById("v1").value;
-     let val2 = document.getElementById("v2").value;
-     let r = Number( val1 ) + Number( val2 );
-     document.getElementById(" resultado ").innerHTML = r;
+  if(!Number(c)){
+    alert("Voce é Gay");
+    document.getElementyById("Valor").value ="";
+    document.getElementById("valor").focus();
+    return
+  }
+
+
+   if(!Number(j)){
+    alert("Voce é Gay");
+    document.getElementyById("Valor").value ="";
+    document.getElementById("valor").focus();
+    return
+  }
+
+  if(!Number(t)){
+    alert("Voce é Gay");
+    document.getElementyById("Valor").value ="";
+    document.getElementById("valor").focus();
+    return
+  }
+let mes= ""
+  for(let i = 1; i <= t; i++){
+      r = c * (1 + (j/100));
+      document.getElementById("mes").innerHTML="mes"+ i +"valor" ;
+     // document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
+      c = r;
+  }
+  document.getElementById("mes").innerHTML=mes;
+
+  document.getElementById("total").innerHTML="Total"+moeda (r);
+  //document.write("Resultado: " + moeda(r));
 }
 
-function subtracao () {
+
+function adicao(){
+let val1 = document.getElementById("v1").value;
+let val2 = document.getElementById("v2").value;
+let r = Number(val1) + Number(val2);
+document.getElementById("resultado").innerHTML =r;
+}
+
+function subtracao(){
+  let val1 = document.getElementById("v1").value;
+  let val2 = document.getElementById("v2").value;
+  let r = Number(val1) - Number(val2);
+  document.getElementById("resultado").innerHTML =r;
+  }
+
+  function divisao(){
     let val1 = document.getElementById("v1").value;
     let val2 = document.getElementById("v2").value;
-    let r = Number( val1 ) - Number( val2 );
-    document.getElementById(" resultado ").innerHTML = r;
-}
+    let r = Number(val1) / Number(val2);
+    document.getElementById("resultado").innerHTML =r;
+    }
+  
+    function multiplicacao(){
+      let val1 = document.getElementById("v1").value;
+      let val2 = document.getElementById("v2").value;
+      let r = Number(val1) * Number(val2);
+      document.getElementById("resultado").innerHTML =r;
+      }
+      function porcentagem(){
+        let val1 = document.getElementById("v1").value;
+        let val2 = document.getElementById("v2").value;
+        let c = 100
+        let p = Number(val2)  / Number(c)
+        let r = Number(val1) * Number(p);
+        document.getElementById("resultado").innerHTML =r;
+        }
